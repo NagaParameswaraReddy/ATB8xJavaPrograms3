45892e660886b3996ad96027682ae515155fb2a5
NagaParameswaraReddy <snagaparameswarareddy@gmail.com>
2024-09-15 00:53:07 +0530

    Add files

---
+++ src/SEPT/EX_13092024_TASK/TASK_OPERATORS.java
@@ -0,0 +1,13 @@
+package SEPT.EX_13092024_TASK;
+
+public class TASK_OPERATORS {
+    public static void main(String[] args){
+        double a=34;
+        double b=10;
+        System.out.printf("%f+%f=%f \n", a,b,a+b);
+        System.out.printf("%f-%f=%f \n", a,b,a-b);
+        System.out.printf("%f*%f=%f \n", a,b,a*b);
+        System.out.printf("%f/%f=%f \n", a,b,a/b);
+
+    }
+}
