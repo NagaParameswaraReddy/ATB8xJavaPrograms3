c34d4942c33d5251559ef3e90a937695cc95f829
NagaParameswaraReddy <snagaparameswarareddy@gmail.com>
2024-09-19 23:24:51 +0530

    Add files via upload

---
+++ src/SEPT/EX_18092024/task3.java
@@ -0,0 +1,9 @@
+package SEPT.EX_18092024;
+
+public class task3 {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a-- +a-- + a--);
+        System.out.println(a);
+    }
+}
