57729ec69edb21bc7531d47e79a614b25eefae69
NagaParameswaraReddy <snagaparameswarareddy@gmail.com>
2024-09-19 22:51:37 +0530

    Add files

---
+++ src/SEPT/EX_09092024_KEYWORDS_AND_IDENTIFIERS/Lab010.java
@@ -0,0 +1,8 @@
+package SEPT.ex_09092024_keywords_and_identifiers;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ src/SEPT/EX_18092024/task1.java
@@ -0,0 +1,11 @@
+package SEPT.EX_18092024;
+
+public class task1 {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(--a + a --  + a--);
+        System.out.println(a);
+
+    }
+
+    }
