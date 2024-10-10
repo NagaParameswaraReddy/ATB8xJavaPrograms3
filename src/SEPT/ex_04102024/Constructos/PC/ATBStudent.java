5c2c07b6fca95cbc4d1edeeedf335d2e5c84074c
NagaParameswaraReddy <snagaparameswarareddy@gmail.com>
2024-10-10 17:04:22 +0530

    Add files via upload

---
+++ src/SEPT/ex_04102024/Constructos/PC/ATBStudent.java
@@ -0,0 +1,14 @@
+package oct.ex_04102024.Constructos.PC;
+
+public class ATBStudent {
+    String name;
+
+    ATBStudent(){
+
+    }
+
+    public static void main(String[] args) {
+        ATBStudent atbStudent = new ATBStudent();
+    }
+
+}
